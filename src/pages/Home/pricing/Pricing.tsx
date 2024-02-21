6412c20de907480f3cbe40dd1bf7623c0df250c8
Abdullah149081 <masudmd425@gmail.com>
2024-02-21 21:36:12 +0600

    Add Pricing and PricingCard components

---
+++ src/pages/Home/pricing/Pricing.tsx
@@ -0,0 +1,9 @@
+const Pricing = () => {
+  return (
+    <div>
+      <h1>This is pricing component</h1>
+    </div>
+  );
+};
+
+export default Pricing;
---
+++ src/pages/Home/pricing/PricingCard.tsx
@@ -0,0 +1,9 @@
+const PricingCard = () => {
+  return (
+    <div>
+      <h1>This is PricingCard component</h1>
+    </div>
+  );
+};
+
+export default PricingCard;
