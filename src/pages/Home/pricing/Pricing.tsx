3c7ca121dd5e1b027879b1b876924f770f8c16cf
Abdullah149081 <masudmd425@gmail.com>
2024-02-23 21:50:44 +0600

    Add pricing section with header text and title

--- src/pages/Home/pricing/Pricing.tsx
+++ src/pages/Home/pricing/Pricing.tsx
@@ -1,8 +1,18 @@
+import HeaderText from '@/components/ui/HeaderText';
+import HeaderTitle from '@/components/ui/HeaderTitle';
+import Container from '../../../components/ui/Container';
+
 const Pricing = () => {
   return (
-    <div>
-      <h1>This is pricing component</h1>
-    </div>
+    <Container className="pb-28">
+      <HeaderTitle>Explore our pricing plans</HeaderTitle>
+      <HeaderText className="mt-6 max-w-[1086px]">
+        Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
+        ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut
+        posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare
+        viverra. Ultrices faucibus neque velit risus ac id lorem.
+      </HeaderText>
+    </Container>
   );
 };
 
