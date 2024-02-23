c4cbc45441a7a37bc70fbcd53a22f125d5dbe7a3
Abdullah149081 <masudmd425@gmail.com>
2024-02-23 21:49:11 +0600

    Add client icons

---
+++ src/assets/client/client.ts
@@ -0,0 +1,23 @@
+import adobe from '@/assets/client/adobe.svg';
+import amazon from '@/assets/client/amazon.svg';
+import asana from '@/assets/client/asana.svg';
+import evernote from '@/assets/client/evernote.svg';
+import google from '@/assets/client/google.svg';
+import microsoft from '@/assets/client/microsoft.svg';
+import paypal from '@/assets/client/paypal.svg';
+import shopify from '@/assets/client/shopify.svg';
+import spotify from '@/assets/client/spotify.svg';
+import uber from '@/assets/client/uber.svg';
+
+export const clients = [
+  spotify,
+  google,
+  uber,
+  microsoft,
+  shopify,
+  evernote,
+  adobe,
+  paypal,
+  amazon,
+  asana,
+];
