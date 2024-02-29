414ce444f7874be21168e22ac3371577ffda9023
Abdullah149081 <masudmd425@gmail.com>
2024-02-29 19:00:53 +0600

    Add new icons and images

---
+++ src/assets/icon/icon.ts
@@ -0,0 +1,11 @@
+import duotonePackage from '@/assets/icon/duotone-package.svg';
+import duotoneRocketLaunch from '@/assets/icon/duotone-rocket-launch.svg';
+import duotoneStack from '@/assets/icon/duotone-stack.svg';
+import fiCheck from '@/assets/icon/fi-check.svg';
+import outlined from '@/assets/icon/outlined-32-arrow-right.svg';
+
+export const duotoneIcon = duotonePackage;
+export const rocketIcon = duotoneRocketLaunch;
+export const stackIcon = duotoneStack;
+export const checkIcon = fiCheck;
+export const arrowRightIcon = outlined;
