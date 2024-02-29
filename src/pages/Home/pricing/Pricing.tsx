56ea1925428e3407e8d6deaf67873c202eeb23fe
Abdullah149081 <masudmd425@gmail.com>
2024-02-29 19:02:43 +0600

    Add pricing cards to Pricing component

--- src/pages/Home/pricing/Pricing.tsx
+++ src/pages/Home/pricing/Pricing.tsx
@@ -1,8 +1,11 @@
 import HeaderText from '@/components/ui/HeaderText';
 import HeaderTitle from '@/components/ui/HeaderTitle';
 import Container from '../../../components/ui/Container';
+import PricingCard from './PricingCard';
+import { plans } from './package';
 
 const Pricing = () => {
+  const plan = plans;
   return (
     <Container className="pb-28">
       <HeaderTitle>Explore our pricing plans</HeaderTitle>
@@ -12,6 +15,11 @@ const Pricing = () => {
         posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare
         viverra. Ultrices faucibus neque velit risus ac id lorem.
       </HeaderText>
+      <div className="mt-16 grid-cols-3 gap-5 lg:grid">
+        <PricingCard plan={plan[0]} />
+        <PricingCard plan={plan[1]} />
+        <PricingCard plan={plan[2]} />
+      </div>
     </Container>
   );
 };
