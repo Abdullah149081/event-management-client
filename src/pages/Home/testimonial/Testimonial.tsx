70d2c335c483d1e9ee86f5322da2443e070ea3a8
Abdullah149081 <masudmd425@gmail.com>
2024-03-04 11:28:29 +0600

    Add TestimonialCard component to Testimonial page

--- src/pages/Home/testimonial/Testimonial.tsx
+++ src/pages/Home/testimonial/Testimonial.tsx
@@ -7,6 +7,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import TestimonialCard from './TestimonialCard';
 
 const Testimonial = () => {
   return (
@@ -15,33 +16,21 @@ const Testimonial = () => {
         What Our Client Said about us
       </HeaderTitle>
 
-      <Carousel className="w-full">
+      <Carousel className="">
         <CarouselContent className="-ml-1">
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem
               key={index}
-              className="pl-1 md:basis-1/2 lg:basis-1/3"
+              className="mt-14 pl-1 md:basis-1/2 lg:basis-[42%]"
             >
-              <div className="p-1">
-                <div className="rounded-lg bg-white p-4 shadow-lg">
-                  <div className="mb-4 flex items-center">
-                    <img
-                      className="h-12 w-12 rounded-full"
-                      src="https://randomuser.me/api/portraits"
-                    />
-                    <div className="ml-4">
-                      <h3 className="text-lg font-semibold">John Doe</h3>
-                      <p className="text-gray-400">CEO</p>
-                      <p className="text-gray-400">Company Name</p>
-                    </div>
-                  </div>
-                </div>
+              <div className="group p-1 ">
+                <TestimonialCard />
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className="!right-12 top-0 " />
-        <CarouselNext className="right-0 top-0" />
+        <CarouselPrevious className="size-[85px] hover:bg-blue-600 hover:text-white" />
+        <CarouselNext className="size-[85px] hover:bg-blue-600 hover:text-white" />
       </Carousel>
     </Container>
   );
