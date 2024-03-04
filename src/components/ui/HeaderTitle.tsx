f40d0c9461a9e43cfc326e0b35b0d922fac9eea0
Abdullah149081 <masudmd425@gmail.com>
2024-03-04 23:09:50 +0600

    Refactor HeaderTitle component to allow custom className

--- src/components/ui/HeaderTitle.tsx
+++ src/components/ui/HeaderTitle.tsx
@@ -1,9 +1,15 @@
+import { cn } from '@/lib/utils';
 import { TChildren } from '@/types';
 
-const HeaderTitle = ({ children }: TChildren) => {
+const HeaderTitle = ({ children, className }: TChildren) => {
   return (
     <div className="mt-20">
-      <h1 className="text-black' text-center font-['Roboto'] text-[64px] font-extrabold capitalize">
+      <h1
+        className={cn(
+          "text-black'  text-center  text-[32px]  font-extrabold capitalize leading-9 md:leading-normal  lg:text-[64px]",
+          className
+        )}
+      >
         {children}
       </h1>
     </div>
