3d485dc9ca15aebdd3a5886c3c19d64670ef6376
Abdullah149081 <masudmd425@gmail.com>
2024-03-05 17:54:39 +0600

    Refactor Home component to include new banner and reorganize sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,31 @@
-import BannerGradient from './BannerGradient';
+import Banner from './banner/Banner';
+import BannerGradient from './banner/BannerGradient';
+import Client from './client/Client';
+import Events from './event/Events';
+import Featured from './featured/Featured';
+import Gallery from './gallery/Gallery';
+import Pricing from './pricing/Pricing';
+import Recent from './recent/Recent';
+import Services from './services/Services';
+import Subscribe from './subscribe/Subscribe';
+import Testimonial from './testimonial/Testimonial';
 
 function Home() {
   return (
     <div className="">
-      <BannerGradient />
+      <div className="relative">
+        <BannerGradient />
+        <Banner />
+      </div>
+      <Client />
+      <Services />
+      <Events />
+      <Pricing />
+      <Gallery />
+      <Testimonial />
+      <Recent />
+      <Featured />
+      <Subscribe />
     </div>
   );
 }
