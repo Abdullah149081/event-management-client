53beb7c63fdf7d61069eaf98320985d5f3f78461
Abdullah149081 <masudmd425@gmail.com>
2024-03-06 22:08:40 +0600

    Refactor Subscribe component layout and styles

--- src/pages/Home/subscribe/Subscribe.tsx
+++ src/pages/Home/subscribe/Subscribe.tsx
@@ -3,23 +3,23 @@ import { Button } from '@/components/ui/button';
 
 const Subscribe = () => {
   return (
-    <div className=" bg-gradient-to-r from-fuchsia-500/20 from-25%  to-cyan-400/20 to-80% py-10   text-slate-900 ">
-      <Container className="">
-        <div>
-          <div className="flex gap-10 ">
-            <div className="flex-2 space-y-3 font-inter ">
-              <h2 className="text-2xl font-extrabold tracking-wide">
+    <div className="pb-28">
+      <div className=" bg-gradient-to-r from-fuchsia-500/20 from-25%  to-cyan-400/20 to-80% py-10   text-slate-900 ">
+        <Container className="">
+          <div className="flex flex-col lg:flex-row lg:gap-10 ">
+            <div className=" space-y-3 font-inter ">
+              <h2 className="text-xs font-extrabold tracking-wide md:text-lg xl:text-2xl">
                 STAY UPDATED ON EVENT 300 ANNOUNCEMENTS
               </h2>
-              <p className="">
+              <p className="text-xs md:text-base">
                 Subscribe for dibs on the latest and greatest happenings.
               </p>
             </div>
-            <div className="mt-4 flex-1">
+            <div className="mt-4  flex-1">
               <div className="flex">
                 <div className="w-full">
                   <input
-                    className="h-full w-full border border-cyan-600 bg-transparent p-4 outline-none placeholder:text-slate-900"
+                    className="h-full w-full border border-cyan-600 bg-transparent p-4 outline-none placeholder:text-sm placeholder:text-slate-900 md:placeholder:text-lg"
                     type="email"
                     placeholder="Enter your email address"
                     name=""
@@ -34,8 +34,8 @@ const Subscribe = () => {
               </div>
             </div>
           </div>
-          <div className="mt-10">
-            <p className="text-sm tracking-wide">
+          <div className="mt-4 md:mt-10">
+            <p className="text-xs tracking-wide md:text-sm">
               We’re committed to your privacy. EVENT 300 uses the information
               you provide to us to contact you about EVENT 300 content and
               events. You may unsubscribe from these communications at any time.
@@ -43,8 +43,8 @@ const Subscribe = () => {
               <span className="cursor-pointer underline">privacy policy.</span>
             </p>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </div>
   );
 };
