6edf80cf4c5cb24d0f30f8e42a1ece89a8c45239
Abdullah149081 <masudmd425@gmail.com>
2024-03-13 23:44:26 +0600

    Imported Toaster component and added it to the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
+import { Toaster } from './components/ui/sonner';
 import './index.css';
 import router from './routes/routes';
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>
 );
