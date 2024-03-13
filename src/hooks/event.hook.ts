945e23c0a8aff44cbf10e80b690f4b404d617d45
Abdullah149081 <masudmd425@gmail.com>
2024-03-13 23:42:22 +0600

    Add event hook for fetching events

---
+++ src/hooks/event.hook.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import axiosClient from '@/network/apiClient.axios';
+
+const fetchEvents = async (limit: string) => {
+  try {
+    const url = limit ? `/events?limit=${limit}` : '/events';
+    const res = await axiosClient.get(url);
+    return res.data;
+  } catch (error) {
+    throw new Error('Failed to fetch events');
+  }
+};
+
+const useEvents = (limit: string) => {
+  const {
+    data: eventData,
+    refetch,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['events', { allEvents: limit }],
+    queryFn: () => fetchEvents(limit),
+  });
+
+  return { eventData, refetch, isError, isLoading };
+};
+
+export default useEvents;
