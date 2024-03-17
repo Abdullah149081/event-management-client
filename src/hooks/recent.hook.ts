0ca99603a6217ae872da8083d9fa1817d34bee12
Abdullah149081 <masudmd425@gmail.com>
2024-03-17 22:44:57 +0600

    Add useRecent hook for fetching recent events

---
+++ src/hooks/recent.hook.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/prefer-default-export */
+
+import axiosClient from '@/network/apiClient.axios';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchRecent = async (limit: string) => {
+  try {
+    const url = limit ? `/recent?limit=${limit}` : '/recent';
+    const res = await axiosClient.get(url);
+    return res.data;
+  } catch (error) {
+    throw new Error('Failed to fetch Recent Event');
+  }
+};
+
+export const useRecent = (limit: string) => {
+  const {
+    data: recentData,
+    refetch,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['recent', { allRecent: limit }],
+    queryFn: () => fetchRecent(limit),
+  });
+
+  return { recentData, refetch, isError, isLoading };
+};
