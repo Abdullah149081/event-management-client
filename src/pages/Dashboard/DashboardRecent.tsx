e5e6ccf8d048a1fb42c78c2cd3e2648983f5f802
Abdullah149081 <masudmd425@gmail.com>
2024-03-18 23:56:07 +0600

    Delete DashboardRecent and DashboardServices components

--- src/pages/Dashboard/DashboardRecent.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardRecent = () => {
-  return (
-    <div>
-      <h1>This is DashBoardRecent component</h1>
-    </div>
-  );
-};
-
-export default DashboardRecent;
--- src/pages/Dashboard/DashboardServices.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardServices = () => {
-  return (
-    <div>
-      <h1>This is DashboardServices component</h1>
-    </div>
-  );
-};
-
-export default DashboardServices;
