adae6c4f33cdfa7fa3aad7856960e5ac782e3c5c
Abdullah149081 <masudmd425@gmail.com>
2024-03-19 17:39:53 +0600

    Refactor ServiceCard component

--- src/pages/Home/services/ServiceCard.tsx
+++ src/pages/Home/services/ServiceCard.tsx
@@ -1,78 +1,61 @@
-import check from '@/assets/icon/fi-check.svg';
 import { Button } from '@/components/ui/button';
+import ServicesItems from './ServicesItems';
 
-const ServiceCard = () => {
+type ServiceCardProps = {
+  service: {
+    name: string;
+    img: string;
+    description: string;
+    services: string[];
+  };
+};
+
+const ServiceCard = ({ service }: ServiceCardProps) => {
+  const { name, img, description, services } = service || {};
+
   return (
     <div className="group relative">
-      <div className="  inline-flex   max-w-[412px] flex-col items-start justify-start gap-6 rounded-md bg-black bg-opacity-5 px-[25px] pb-[30px] pt-[25px] group-hover:bg-transparent ">
+      <div className=" flex h-full   max-w-[412px] flex-col items-start justify-start gap-6 rounded-md bg-black bg-opacity-5 px-[25px] pb-[30px] pt-[25px] group-hover:bg-transparent ">
         {/* card  */}
-        <div className="  group-hover:opacity-0  ">
+        <div className="  group-hover:opacity-0 ">
           <img
             className="h-[221.20px] w-[316.50px] rounded-md object-cover  md:h-[187.17px] md:w-[267.81px] xl:h-[253px] xl:w-[362px]"
-            src="https://via.placeholder.com/362x253"
+            src={img}
             alt=""
           />
-          <div className="mt-6 inline-flex flex-col items-start justify-start gap-3 self-stretch">
+          <div className="mt-6 flex flex-col items-start justify-start gap-3 self-stretch">
             <div className="flex h-[35px] flex-col items-start justify-start gap-2 self-stretch">
-              <div className="text-center   text-[27.98px] font-extrabold  leading-[30.78px]  text-black  md:text-2xl md:leading-relaxed  xl:text-[32px] xl:leading-9">
-                Corporate event
-              </div>
+              <h2 className="text-center text-[27.98px]  font-extrabold capitalize  leading-[30.78px]  text-black  md:text-2xl md:leading-relaxed  xl:text-[32px] xl:leading-9">
+                {name.charAt(0).toLocaleUpperCase() +
+                  name.slice(1).toLowerCase()}
+              </h2>
             </div>
-            <div className="mt-3 flex flex-col items-start justify-start gap-4">
-              <div className="inline-flex items-center justify-start gap-[18px]">
-                <div className="relative h-5 w-5">
-                  <div className="absolute left-0 top-0 h-5 w-5 rounded-full bg-white" />
-                  <img
-                    src={check}
-                    className="absolute left-[5px] top-[5px] "
-                    alt="check"
-                  />
-                </div>
-                <div className="font-plus  text-base font-normal leading-normal text-black md:text-sm md:leading-tight xl:text-lg xl:leading-[27px]">
-                  One day pas access all lecture
-                </div>
-              </div>
-            </div>
+            <ul className="mt-3 flex flex-col items-start justify-start gap-4 ">
+              {services.map((item, index) => (
+                <ServicesItems key={index} service={item} />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
       {/* hover */}
       <div className=" absolute  top-0 z-20 transform rounded-md  bg-blue-500/40 px-4  py-8 opacity-0   shadow transition duration-500 ease-in-out group-hover:opacity-100 md:max-w-[304.80px] xl:max-w-[412px] xl:bg-blue-500/5 xl:pl-7">
-        <div className=" inline-flex flex-col items-start justify-start gap-3">
+        <div className=" flex flex-col items-start justify-start gap-3">
           <div className="flex h-[35px] flex-col items-start justify-start gap-2 self-stretch">
-            <div className=" text-center  text-[27.98px] font-extrabold  leading-[30.78px] text-black md:text-2xl md:leading-relaxed  xl:text-[32px]  xl:leading-9">
-              Wedding event
-            </div>
+            <h2 className=" text-center  text-[27.98px] font-extrabold  leading-[30.78px] text-black md:text-2xl md:leading-relaxed  xl:text-[32px]  xl:leading-9">
+              {name.charAt(0).toLocaleUpperCase() + name.slice(1).toLowerCase()}
+            </h2>
           </div>
-          <div className=" text-sm  leading-snug  tracking-wide   text-slate-500   md:text-xs md:leading-[18.94px] xl:max-w-[342px] xl:text-base xl:leading-relaxed">
-            Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
-            ornare viverra. Ultrices faucibus neque velit risus ac id lorem.Ut
-            posuere felis arcu tellus tempus in in ultricies.Ut posuere felis
-            arcu tellus tempus in in ultricies. Ut posuere felis arcu tellus
-            tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices
-            faucibus neque velit risus ac id lorem.Ut posuere felis arcu tellus
-            tempus in in ultricies.Ut posuere felis arcu tellus tempus in in
-            ultricies.
-          </div>
+          <p className=" text-sm  leading-snug  tracking-wide   text-slate-500   md:text-xs md:leading-[18.94px] xl:max-w-[342px] xl:text-base xl:leading-relaxed">
+            {description}
+          </p>
         </div>
-        <div className="mt-8  inline-flex flex-col items-start justify-start gap-4 ">
-          <div className="flex flex-col items-start justify-start gap-4">
-            <div className="inline-flex items-center justify-start gap-[18px]">
-              <div className="relative h-5 w-5">
-                <div className="absolute left-0 top-0 h-5 w-5 rounded-full bg-white" />
-                <img
-                  src={check}
-                  alt="check"
-                  className="absolute left-[5px] top-[5px] h-2.5 w-2.5"
-                />
-              </div>
-              <div className=" font-plus  text-base font-normal leading-normal text-black md:text-sm md:leading-tight xl:text-lg xl:leading-[27px]">
-                One day pas access all lecture
-              </div>
-            </div>
-          </div>
-        </div>
-        <Button className="mt-12  inline-flex h-[52px] w-full items-center justify-center gap-2.5 rounded-none bg-blue-600  py-6 font-bold   xl:w-[340px]">
+        <ul className="mt-8 flex flex-col items-start justify-start gap-4 ">
+          {services.map((item, index) => (
+            <ServicesItems key={index} service={item} />
+          ))}
+        </ul>
+        <Button className="mt-12  flex h-[52px] w-full items-center justify-center gap-2.5 rounded-none bg-blue-600  py-6 font-bold   xl:w-[340px]">
           Check it out
         </Button>
       </div>
