9381e52d9882a24a27f5b0692de9e98c7df7ef53
Abdullah149081 <masudmd425@gmail.com>
2024-03-19 17:40:21 +0600

    Refactor Services component and add loading and error handling

--- src/pages/Home/services/Services.tsx
+++ src/pages/Home/services/Services.tsx
@@ -1,21 +1,28 @@
 import Container from '@/components/ui/Container';
 import HeaderTitle from '@/components/ui/HeaderTitle';
+import Loading from '@/components/ui/loading';
+import { useServices } from '@/hooks';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
+  const { servicesData, isError, isLoading } = useServices('3');
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Loading error />;
   return (
     <Container className="relative z-10  xl:pb-28 ">
       <div className="absolute inset-0   -z-10 rounded-xl    bg-gradient-to-r from-fuchsia-500/20 from-25%  to-cyan-400/20 to-80% blur-[150px]   " />
       <HeaderTitle>Our Services</HeaderTitle>
       <div className="mx-auto w-[327px] text-center text-sm  font-normal leading-snug text-slate-500  md:w-[518.61px] xl:w-[701px] xl:text-lg xl:leading-[28.80px]">
-        Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
-        ornare viverra. Ultrices faucibus neque velit risus ac id lorem.
+        Comprehensive event management services from concept to execution,
+        ensuring seamless experiences that exceed expectations and leave lasting
+        impressions
       </div>
       <div className="relative  mt-14">
         <div className=" grid justify-center gap-5  md:grid-cols-3">
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
+          {servicesData.map((service) => (
+            <ServiceCard key={service._id} service={service} />
+          ))}
         </div>
       </div>
     </Container>
