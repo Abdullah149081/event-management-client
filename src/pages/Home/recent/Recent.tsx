c7975409d1158b9cc5f324c7b83665eb5a452445
Abdullah149081 <masudmd425@gmail.com>
2024-03-20 16:00:02 +0600

    Fix recentData null check

--- src/pages/Home/recent/Recent.tsx
+++ src/pages/Home/recent/Recent.tsx
@@ -20,14 +20,14 @@ const Recent = () => {
       <div className=" flex flex-1 flex-col items-start justify-start gap-5">
         <div className="relative flex flex-col items-start justify-start gap-5 md:flex-row ">
           <div className="absolute right-0 -z-10 h-full w-[300px]  rounded-full bg-pink-500/25 blur-[244px] md:left-0 md:h-[650px] md:w-[500px] md:rotate-[40.34deg] xl:h-[1017.96px]   xl:w-[749.95px]" />
-          <RecentImage src={recentData[0].img} className="rounded-[26.34px]" />
-          <RecentImage src={recentData[1].img} className="rounded-[26.21px]" />
-          <RecentImage src={recentData[2].img} className="rounded-[114px]" />
+          <RecentImage src={recentData[0]?.img} className="rounded-[26.34px]" />
+          <RecentImage src={recentData[1]?.img} className="rounded-[26.21px]" />
+          <RecentImage src={recentData[2]?.img} className="rounded-[114px]" />
         </div>
         <div className="flex flex-col items-start justify-start gap-5 md:flex-row">
-          <RecentImage src={recentData[3].img} className="rounded-[114px]" />
-          <RecentImage src={recentData[4].img} className="rounded-[26.07px]" />
-          <RecentImage src={recentData[5].img} className="rounded-[26.21px]" />
+          <RecentImage src={recentData[3]?.img} className="rounded-[114px]" />
+          <RecentImage src={recentData[4]?.img} className="rounded-[26.07px]" />
+          <RecentImage src={recentData[5]?.img} className="rounded-[26.21px]" />
         </div>
       </div>
       <div className="flex flex-1 flex-col items-center justify-center  xl:items-start xl:justify-start xl:gap-12">
@@ -38,7 +38,7 @@ const Recent = () => {
             </div>
           </div>
           <div className="w-[295px] text-center  text-sm font-normal leading-snug  text-slate-500 md:w-[640px] md:text-lg  md:leading-[28.80px] xl:w-full xl:self-stretch xl:text-start">
-            {recentData[0].description}
+            {recentData[0]?.description}
           </div>
         </div>
         <Button className="  w-[174px] rounded-none bg-blue-600 px-10 py-6 font-plus font-bold">
