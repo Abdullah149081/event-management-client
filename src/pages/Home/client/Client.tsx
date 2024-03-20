db26742f64645e4c5e201287c0edd6d4d10d6f1f
Abdullah149081 <masudmd425@gmail.com>
2024-03-20 15:58:50 +0600

    Fix rendering issue with clients array

--- src/pages/Home/client/Client.tsx
+++ src/pages/Home/client/Client.tsx
@@ -7,7 +7,7 @@ const Client = () => {
     <Container className="pb-16 xl:pb-32">
       <HeaderTitle>Our Valuable client</HeaderTitle>
       <div className="mt-10 grid grid-cols-2 gap-10 md:mt-20 md:grid-cols-4 md:gap-[94px] xl:grid-cols-5">
-        {clients.map((client, index) => (
+        {clients?.map((client, index) => (
           <img key={index} src={client} alt={client} />
         ))}
       </div>
