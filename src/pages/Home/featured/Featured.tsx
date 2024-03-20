2091a13ab56b1caddc5113377c8824f044f4e789
Abdullah149081 <masudmd425@gmail.com>
2024-03-20 21:44:06 +0600

    Refactor Featured component and FeaturedCard component

--- src/pages/Home/featured/Featured.tsx
+++ src/pages/Home/featured/Featured.tsx
@@ -7,9 +7,16 @@ import {
   CarouselItem,
 } from '@/components/ui/carousel';
 
+import Loading from '@/components/ui/loading';
+import { useFeatured } from '@/hooks';
+import { TFeatured } from '@/types';
 import FeaturedCard from './FeaturedCard';
 
 const Featured = () => {
+  const { featured, isError, isLoading } = useFeatured();
+
+  if (isError) return <Loading error />;
+
   return (
     <Container className="pb-16 xl:pb-32">
       <HeaderTitle className="mx-0 px-0 text-start ">
@@ -20,20 +27,24 @@ const Featured = () => {
         create new ones.
       </HeaderText>
 
-      <Carousel className="">
-        <CarouselContent className="-ml-1 ">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              className="mt-14 rounded bg-slate-400/5 py-5 pl-1 shadow-2xl md:basis-1/2 lg:basis-2/5 xl:basis-1/4 "
-            >
-              <div className="group p-1 ">
-                <FeaturedCard />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Carousel className="">
+          <CarouselContent className="-ml-1 ">
+            {featured.map((feature: TFeatured) => (
+              <CarouselItem
+                key={feature._id}
+                className="mt-14 rounded bg-slate-400/5 py-5 pl-1 shadow-2xl md:basis-1/2 lg:basis-2/5 xl:basis-1/4 "
+              >
+                <div className="group h-full p-1">
+                  <FeaturedCard feature={feature} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      )}
     </Container>
   );
 };
--- src/pages/Home/featured/FeaturedCard.tsx
+++ src/pages/Home/featured/FeaturedCard.tsx
@@ -1,21 +1,26 @@
 import { Button } from '@/components/ui/button';
+import { TFeatured } from '@/types';
 
-const FeaturedCard = () => {
+const FeaturedCard = ({ feature }: { feature: TFeatured }) => {
+  const { img, follow, title } = feature || {};
+
   return (
-    <div className="flex flex-col items-center justify-center rounded-xl bg-white/20 px-4 py-10  shadow">
+    <div className="flex h-full flex-col items-center justify-center rounded-xl bg-white/20 px-4 py-10  shadow">
       <div className="flex flex-col items-center justify-center gap-5">
         <div className="">
-          <img
-            src="https://via.placeholder.com/85x85"
-            alt=""
-            className=" size-[70.5px] rounded-full"
-          />
+          <figure className="rounded-full ring-2 ring-primary ring-offset-2">
+            <img
+              src={img}
+              alt={title}
+              className=" size-[85.5px] rounded-full"
+            />
+          </figure>
         </div>
         <div className="flex flex-col items-center justify-center gap-2">
           <h2 className="text-center  font-plus font-bold text-[#39364f] md:text-[20px]">
-            GCEDIM | Posgrados y Educación Continua
+            {title}
           </h2>
-          <p className="text-lg text-[#6f7287]">9722 followers</p>
+          <p className="text-lg text-[#6f7287]">{follow} followers</p>
         </div>
       </div>
       <div className="mt-2 xl:mt-10">
